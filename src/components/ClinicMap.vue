32630291b9633b9da53b142b731a6d80a9b81932
Shuwei Fang <sfang@homeaway.com>
2017-03-19 16:48:39 +0800

    Add basic map

--- src/components/ClinicMap.vue
+++ src/components/ClinicMap.vue
@@ -1,17 +1,43 @@
 <template>
-  <div class="map">
-
-  </div>
+  <gmap-map
+    :center="center"
+    :zoom="7"
+    style="width: 500px; height: 300px"
+  >
+    <gmap-marker
+      v-for="m in markers"
+      :position="m.position"
+      :clickable="true"
+      :draggable="true"
+      @click="center=m.position"
+    ></gmap-marker>
+  </gmap-map>
 </template>
 
 <script>
-export default {
-  name: 'hello',
-  data () {
-    return {
+  import * as VueGoogleMaps from 'vue2-google-maps';
+  import Vue from 'vue';
+
+  Vue.use(VueGoogleMaps, {
+    load: {
+      key: 'key'
+      // libraries: 'places', //// If you need to use place input
+    }
+  });
+
+  export default {
+    name: 'hello',
+    data () {
+      return {
+        center: {lat: 10.0, lng: 10.0},
+        markers: [{
+          position: {lat: 10.0, lng: 10.0}
+        }, {
+          position: {lat: 11.0, lng: 11.0}
+        }]
+      }
     }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
