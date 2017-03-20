4e3ef3bdbc67beee824373fe3a85a59984ed4187
Shuwei Fang <sfang@homeaway.com>
2017-03-20 12:38:33 +0800

    Add response for Letsencrypt

--- dist/server.js
+++ dist/server.js
@@ -3,6 +3,9 @@ var path = require('path');
 var serveStatic = require('serve-static');
 app = express();
 app.use(serveStatic(__dirname));
+app.get('/.well-known/acme-challenge/:content', function(req, res) {
+  res.send('ueBUqEao6r5W90kS2jTvOOMk4kjYdrV_pjBCDdxfuBw.Vf0hm5biDb_O0X6nZjmsPin5O_8GdOhK5ynsw30TBsQ')
+})
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
