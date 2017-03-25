b9fcbf0b745e84bf4aa0c473032fd02bc3c3614e
Shuwei Fang <sfang@homeaway.com>
2017-03-25 14:18:38 +0800

    Show marker & info window on map based on clinic data

--- src/components/ClinicMap.vue
+++ src/components/ClinicMap.vue
@@ -1,27 +1,22 @@
 <template>
   <gmap-map
     :center="center"
-    :zoom="7"
-    style="width: 500px; height: 300px"
+    :zoom="zoom"
+    style="width: 100%; height: 500px"
   >
-    <gmap-marker
-      v-for="m in markers"
-      :position="m.position"
-      :clickable="true"
-      :draggable="true"
-      @click="center=m.position"
-    ></gmap-marker>
+    <gmap-info-window :options="infoOptions" :position="infoWindowPos" :opened="infoWinOpen" :content="infoContent" @closeclick="infoWinOpen=false"></gmap-info-window>
+    <gmap-marker v-for="(m,i) in markers" :position="m.position" :clickable="true" @click="toggleInfoWindow(m,i)"></gmap-marker>
   </gmap-map>
 </template>
 
 <script>
   import * as VueGoogleMaps from 'vue2-google-maps';
   import Vue from 'vue';
+  import parse from 'csv-parse';
 
   Vue.use(VueGoogleMaps, {
     load: {
       key: 'key'
-      // libraries: 'places', //// If you need to use place input
     }
   });
 
@@ -30,36 +25,61 @@
     data () {
       return {
         center: {lat: 1.29, lng: 103.82},
+        infoOptions: {
+          pixelOffset: {
+            width: 0,
+            height: -35
+          }
+        },
+        zoom: 11,
+        infoWindowPos: {
+          lat: 0,
+          lng: 0
+        },
+        infoWinOpen: false,
+        infoContent: '',
+        currentMidx: null,
         markers: [],
         clinics: []
       }
     },
     mounted () {
+      this.getLocation()
       this.readClinicFile()
-
     },
     methods: {
+      getLocation() {
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition((position) => {
+            this.center = {lat: position.coords.latitude, lng: position.coords.longitude}
+            this.zoom = 13
+          })
+        } else {
+          x.innerHTML = "Geolocation is not supported by this browser.";
+        }
+      },
       readClinicFile() {
         var rawFile = new XMLHttpRequest();
         var vm = this;
-        rawFile.open("GET", '../../static/test.csv', false);
+        rawFile.open("GET", '../../static/output.csv', false);
         rawFile.onreadystatechange = function ()
         {
           if(rawFile.readyState === 4)
           {
             if(rawFile.status === 200 || rawFile.status == 0)
             {
-              var allText = rawFile.responseText.split('~~~')
-              vm.processFile(allText)
-              vm.addMarkersToMap()
+              parse(rawFile.responseText, function(err, output){
+                vm.processFile(output)
+                vm.addMarkersToMap()
+              });
             }
           }
         }
         rawFile.send(null);
       },
       processFile(input) {
-        for (var i = 1; i < input.length-1; i++) {
-          let line = input[i].split(',')
+        input.forEach(line => {
+          console.log(line)
           this.clinics.push({
             code: line[2],
             name: line[3],
@@ -71,27 +91,43 @@
             sat: line[9],
             sun: line[10],
             pb: line[11],
-            remark: line[12]
+            remark: line[12],
+            lat: Number(line[13]),
+            lng: Number(line[14])
           })
-        }
+        })
       },
       addMarkersToMap() {
         let vm = this
-        VueGoogleMaps.loaded.then(function() {
-          let geocoder = new google.maps.Geocoder()
-          vm.clinics.forEach(function(clinic) {
-            geocoder.geocode( { 'address': clinic.address}, function(results, status) {
-              if (status == 'OK') {
-                let lat = results[0].geometry.location.lat()
-                let lng = results[0].geometry.location.lng()
-                vm.markers.push({position: {lat: lat, lng: lng}})
-              } else {
-                console.log(clinic.address, 'Geocode was not successful for the following reason: ' + status);
-              }
-            });
+        vm.clinics.forEach(clinic => {
+          vm.markers.push({
+            position: {lat: clinic.lat, lng: clinic.lng},
+            infoText: `<div><b> ${clinic.name}</b></div>` +
+            `<div>Address: ${clinic.address}</div>` +
+            `<div>Postal code: ${clinic.postal}</div>` +
+            `<div>Tel: ${clinic.tel}</div>` +
+            `<div>Fax: ${clinic.fax}</div>` +
+            `<div>Opening hours: </div>` +
+            `<div>Mon - Fri: ${clinic.weekday}</div>` +
+            `<div>Sat: ${clinic.sat}</div>` +
+            `<div>Sun: ${clinic.sun}</div>` +
+            `<div>Public holiday: ${clinic.pb}</div>`
           })
 
         })
+      },
+      toggleInfoWindow: function(marker, idx) {
+        this.infoWindowPos = marker.position;
+        this.infoContent = marker.infoText;
+        //check if its the same marker that was selected if yes toggle
+        if (this.currentMidx == idx) {
+          this.infoWinOpen = !this.infoWinOpen;
+        }
+        //if different marker set infowindow to open and reset current marker index
+        else {
+          this.infoWinOpen = true;
+          this.currentMidx = idx;
+        }
       }
     }
   }
